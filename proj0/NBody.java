ec9035382b59e3f8714a1a553a13b4ba47efab4b
Ashley <ashleyshi006@gmail.com>
2023-03-06 15:18:39 -0500

    fix print out bug in proj0

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -61,11 +61,12 @@ public class NBody {
 			t += dt;
 		}
 		
-		StdOut.printf("%d\n", planets.length)
-		StdOut.printf("%.2e\n", radius);
+		StdOut.printf("%d\n", planets.length);
+		StdOut.printf("%.2e\n", r);
 		for (int i = 0; i < planets.length; i++) {
 		    StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
 		                  planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
-		                  planets[i].yyVel, planets[i].mass, planets[i].imgFileName); 
+		                  planets[i].yyVel, planets[i].mass, planets[i].imgFileName);   
+		}						
 	}
 }
