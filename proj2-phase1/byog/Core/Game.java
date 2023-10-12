bc724e3a378d1ff54daf7d8219ce44d78d9e4f8e
Ashley <ashleyshi006@gmail.com>
2023-10-11 21:30:09 -0400

    re-add proj2 phase1

---
+++ proj2-phase1/byog/Core/Game.java
@@ -0,0 +1,67 @@
+package byog.Core;
+
+import byog.TileEngine.TERenderer;
+import byog.TileEngine.TETile;
+import byog.TileEngine.Tileset;
+
+import java.util.Random;
+
+public class Game {
+    TERenderer ter = new TERenderer();
+    /* Feel free to change the width and height. */
+    public static final int WIDTH = 81;
+    public static final int HEIGHT = 31;
+
+    /**
+     * Method used for playing a fresh game. The game should start from the main menu.
+     */
+    public void playWithKeyboard() {
+    }
+
+    /**
+     * Method used for autograding and testing the game code. The input string will be a series
+     * of characters (for example, "n123sswwdasdassadwas", "n123sss:q", "lwww". The game should
+     * behave exactly as if the user typed these characters into the game after playing
+     * playWithKeyboard. If the string ends in ":q", the same world should be returned as if the
+     * string did not end with q. For example "n123sss" and "n123sss:q" should return the same
+     * world. However, the behavior is slightly different. After playing with "n123sss:q", the game
+     * should save, and thus if we then called playWithInputString with the string "l", we'd expect
+     * to get the exact same world back again, since this corresponds to loading the saved game.
+     * @param input the input string to feed to your program
+     * @return the 2D TETile[][] representing the state of the world
+     */
+
+
+    public TETile[][] playWithInputString(String input) {
+        long seed = Long.parseLong(input.replaceAll("[^0-9]", ""));
+        Random RANDOM = new Random(seed);
+        TETile[][] world = worldInitialize();
+        return WorldCreator.worldGenerator(RANDOM, world);
+    }
+
+
+    /**
+     * Reference: https://github.com/stg1205/CS61B/tree/master/proj2/byog/Core
+     * programming for display
+     * public void playWithInputString(String input) {
+     * TERenderer ter = new TERenderer();
+     * ter.initialize(WIDTH, HEIGHT);
+     * <p>
+     * long seed = Long.parseLong(input.replaceAll("[^0-9]", ""));
+     * Random RANDOM = new Random(seed);
+     * <p>
+     * TETile[][] world = worldInitialize();
+     * ter.renderFrame(WorldCreator.worldGenerator(RANDOM, world));
+     * }
+     */
+
+    private TETile[][] worldInitialize() {
+        TETile[][] world = new TETile[WIDTH][HEIGHT];
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                world[x][y] = Tileset.NOTHING;
+            }
+        }
+        return world;
+    }
+}
